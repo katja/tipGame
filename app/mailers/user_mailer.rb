fe376b5771963ed9b4595da9668e8b0373a8b466
katja <ich-bin-sowieso@abwesend.de>
2010-06-16 17:49:15 +0200

    used more suitable words for messages

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,11 @@
 class UserMailer < ActionMailer::Base
-  default :from => "info@titut.de"
-  #default_url_options[:host] = "titut.de"
+  default :from => "info@alles-und-nicht-mehr.de"
   
   def registration_confirmation(user)
     @user = user
     mail(:to => "#{user.name} <#{user.mail}>",
-         :bcc => "info@titut.de",
-         :from => "TipWM",
+         :bcc => "info@alles-und-nicht-mehr.de",
+         :from => "TippSpiel <info@alles-und-nicht-mehr.de>",
          :subject => "Willkommen bei TipWM" )
   end
   
@@ -14,8 +13,8 @@ class UserMailer < ActionMailer::Base
     @user = user
     @password_reset_url = edit_password_reset_url(user.perishable_token)
     mail(:to => "#{user.name} <#{user.mail}>",
-         :bcc => "info@titut.de",
-         :from => "Titut",
-         :subject => "titut.de - Neues Passwort anlegen")
+         :bcc => "info@alles-und-nicht-mehr.de",
+         :from => "Tippspiel",
+         :subject => "TippSpiel.de - Neues Passwort anlegen")
   end
 end
